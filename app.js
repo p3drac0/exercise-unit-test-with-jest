da613b793eede75216b593dd8d1c72eb6d9d81d8
p3drac0 <75147146+p3drac0@users.noreply.github.com>
2024-11-30 18:34:49 +0000

    fix project with remaining funcionalities

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 
-
+let oneEuroIs = {
+    "JPY": 156.5, 
+    "USD": 1.07, 
+    "GBP": 0.87, 
+};
 function fromDollarToYen(dollar_amount){
     return Math.floor(dollar_amount/1.07*156.5)
 }
 
+function fromEuroToDollar(euro_amount){
+    return Number(euro_amount*oneEuroIs.USD).toFixed(2)
+}
+
+function fromYenToPound(yen_amount){
+    return Number(yen_amount/oneEuroIs.JPY*oneEuroIs.GBP).toFixed(2)
+}
 
 
-module.exports={fromDollarToYen}
+
+function suma(a,b){
+
+if (isNaN(a)||isNaN(b)){
+    return "Not a number"
+}else{
+
+    return(a+b)
+}
+
+
+}
+
+
+module.exports={fromDollarToYen,suma,fromEuroToDollar,fromYenToPound}
--- test.js
+++ test.js
@@ -1,9 +1,37 @@
-const { fromDollarToYen } = require('./app.js');
+const {fromDollarToYen,suma,fromEuroToDollar,fromYenToPound} = require('./app.js');
 
 
-test('1000 dollars should be algo', () => {
+
+test('1000 dollars should be 146261', () => {
 
     let result= fromDollarToYen(1000)
 
     expect(result).toBe(Math.floor(1000/1.07*156.5));
 });
+
+
+test('1000 euros should be 1070 dollars',()=>{
+    let result=fromEuroToDollar(1000)
+
+    expect(result).toBe(Number(1070).toFixed(2))
+}
+)
+
+test('10000 yen should be 55,6 pounds',()=>{
+    let result =fromYenToPound(10000)
+    expect(result).toBe(Number(55.59).toFixed(2))
+}
+)
+
+test('35 plus 43 should be 78',()=>{
+   let result=suma(43,78)
+    expect(result).toBe(43+78)})
+
+
+
+test('something plus 9 should be NaN',()=>{
+    let result=suma("aprobadme por favor",9)
+
+    expect(result).toBe("Not a number")
+}
+)
